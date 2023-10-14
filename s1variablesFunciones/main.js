792c94227b87bc8324b2ac17d041feb8e7bcbd7f
angelagar <ggarcia.angelad@gmail.com>
2023-10-13 22:11:21 -0600

    first 2 practices of javascript.

---
+++ s1variablesFunciones/main.js
@@ -0,0 +1,15 @@
+console.log("hola koders")
+console.log( "sudo")
+
+let a= 15;
+let b= 6;
+
+let result1 = a + b
+let result2 = a-b
+let result3 = a * b
+let result4 = a % b
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
+console.log(result4)
---
+++ s2Operadoresyfunciones/main.js
@@ -0,0 +1,60 @@
+function additionOf2(a,b){
+    let resultAddition= a+b;
+    return resultAddition
+}
+
+let sum1 = additionOf2(14,5);
+console.log(sum1);
+
+function sustractionOf2(a,b){
+    let resultSustraction= a-b;
+    return resultSustraction
+}
+
+let rest1 = sustractionOf2(8,3);
+console.log(rest1);
+
+function multiplicationOf2(a,b){
+    let resultMult= a+b;
+    return resultMult
+}
+
+let mult1 = multiplicationOf2(7,5);
+console.log(mult1);
+
+function divisionOf2(a,b){
+    let resultDiv= a/b;
+    return resultDiv
+}
+
+let div1 = divisionOf2(30,6);
+console.log(div1);
+
+function moduleOf2(a,b){
+    let resultModule= a%b;
+    return resultModule
+}
+
+let module1 = moduleOf2(34,6);
+console.log(module1);
+
+function greaterThan(a, b){
+    if(a > b){
+        return `${a} es mayor`
+    }else{
+        return `${b} es mayor`
+    }
+}
+    
+console.log(greaterThan(545,2222))
+    
+    
+function pairOdd(a){
+    if(a % 2 == 0){
+        return `${a} es par`
+        }else{
+        return `${a} es non`
+    }
+} 
+    
+console.log(pairOdd(13))
